d42934ea5f4deff3b6f43cdcd60e0bca23537c9a
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-18 21:34:02 +0700

    tugas 13 js, Muhammad Rizwan

---
+++ tugas13.js
@@ -0,0 +1,13 @@
+/* Tugas 13 Javascript */
+// Muhammad Rizwan
+
+function tugasTigaBelas(){
+    var buah = ['Pisang','Jeruk'];
+    console.log(buah);
+
+    buah.unshift('Apel','Mangga');
+
+    return buah;
+}
+
+console.log(tugasTigaBelas());
